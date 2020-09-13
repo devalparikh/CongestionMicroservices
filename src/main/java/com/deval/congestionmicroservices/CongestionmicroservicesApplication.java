3a34fd2f66044a0355927672a832d85d3a481101
Deval Parikh <devalpp@gmail.com>
2020-09-12 23:29:17 -0400

    init

---
+++ src/main/java/com/deval/congestionmicroservices/CongestionmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.deval.congestionmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongestionmicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongestionmicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deval/congestionmicroservices/CongestionmicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deval.congestionmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongestionmicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
